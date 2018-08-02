741c6865d1ccc2abfdaae74bd374cce6bfc2a261
WIN-838PA7AILD8\Administrator <hejia.song@zhaopin.com.cn>
2018-08-02 19:15:03 +0800

    update

--- packages/index.js
+++ packages/index.js
@@ -1,6 +1,6 @@
 // This file is auto gererated by build/build-entry.js
 import Hello from './hello';
-const version = '0.1.14';
+const version = '0.1.15';
 const components = [
   Hello
 ];
---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import { HpComponent } from './component';
+
+export const version: string
+export function install(vue: typeof Vue): void
+export class Hello extends HpComponent { }
