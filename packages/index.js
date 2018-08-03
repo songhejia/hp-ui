4e9d8e601e1411b074ff5582b64a39d3c22d55aa
WIN-838PA7AILD8\Administrator <hejia.song@zhaopin.com.cn>
2018-08-03 11:19:23 +0800

    update

--- packages/index.js
+++ packages/index.js
@@ -1,7 +1,7 @@
 // This file is auto gererated by build/build-entry.js
 import Ahello from './ahello';
 import Hello from './hello';
-const version = '0.1.21';
+const version = '0.1.22';
 const components = [
   Ahello,
   Hello
