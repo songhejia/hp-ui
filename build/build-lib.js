ccb34fba027f71a06633fd4cbd5ad7e57a153450
WIN-838PA7AILD8\Administrator <hejia.song@zhaopin.com.cn>
2018-08-07 17:49:17 +0800

    [release] Auto-commit

--- build/build-lib.js
+++ build/build-lib.js
@@ -11,12 +11,12 @@ const tasks = [
 ];
 
 tasks.forEach(task => {
-  signale.start(task);
+  // signale.start(task);
 
-  const interactive = new Signale({ interactive: true });
-  interactive.pending(task);
+  // const interactive = new Signale({ interactive: true });
+  // interactive.pending(task);
   const cmd = `npm run ${task} --silent`;
   console.log('执行命令：', cmd)
   shell.exec(cmd);
-  interactive.success(task);
+  // interactive.success(task);
 });
