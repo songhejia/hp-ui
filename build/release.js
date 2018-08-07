55129bf245d04dba98cd793f57ed64b935849edc
WIN-838PA7AILD8\Administrator <hejia.song@zhaopin.com.cn>
2018-08-07 16:42:33 +0800

    commit

--- build/release.js
+++ build/release.js
@@ -8,7 +8,10 @@ if (!shell.which('git')) {
 }
 
 shell.echo('start 提交代码')
-shell.exec('git add .')
+if (shell.exec('git add .').code !== 0) {
+  shell.echo('Error: Git add failed');
+  shell.exit(1);
+}
 if (shell.exec('git commit -m "[release] Auto-commit"').code !== 0) {
   shell.echo('Error: Git commit failed');
   shell.exit(1);
@@ -17,16 +20,25 @@ shell.echo('end 提交代码')
 
 
 shell.echo('start 版本自增')
-shell.exec('git version patch')
-shell.echo('start 版本自增')
+if (shell.exec('npm version patch').code !== 0) {
+  shell.echo('Error: npm version failed');
+  shell.exit(1);
+}
+shell.echo('end 版本自增')
 
 
 shell.echo('start  编译代码')
 
-shell.exec('npm run build:lib')
-shell.echo('start 编译代码')
+if (shell.exec('npm run build:lib').code !== 0) {
+  shell.echo('Error: npm run build:lib failed');
+  shell.exit(1);
+}
+shell.echo('end 编译代码')
 
 
 shell.echo('start  发布npm')
-shell.exec('npm publish')
-shell.echo('start 发布npm')
+if (shell.exec('npm publish').code !== 0) {
+  shell.echo('Error: npm publish failed');
+  shell.exit(1);
+}
+shell.echo('end 发布npm')
