f44c7b8b22acec8ca9eb4f1d4c7de6f6da2a7ce1
WIN-838PA7AILD8\Administrator <hejia.song@zhaopin.com.cn>
2018-08-07 17:29:48 +0800

    commit

--- build/release.js
+++ build/release.js
@@ -1,45 +1,55 @@
-var shell = require('shelljs');
+require('shelljs/global')
 
-// shell.cd('../')
-shell.echo(shell.pwd())
-if (!shell.which('git')) {
-  shell.echo('Sorry, this script requires git');
-  shell.exit(1);
+echo(pwd())
+var path = pwd()
+if (exec(`git commit -am "[release] Auto-commit" -F ${path}`).code !== 0) {
+  echo('Error: Git commit failed');
+  exit(1);
 }
+// var shell = require('shelljs');
 
-shell.echo('start 提交代码')
-if (shell.exec('git add .').code !== 0) {
-  shell.echo('Error: Git add failed');
-  shell.exit(1);
-}
-if (shell.exec('git commit -m "[release] Auto-commit"').code !== 0) {
-  shell.echo('Error: Git commit failed');
-  shell.exit(1);
-}
-shell.echo('end 提交代码')
+// // shell.cd('../')
+// shell.echo(shell.pwd())
+// var git = encodeURIComponent(shell.which('git').replace(/\\/g, '/'))
+// if (!git) {
+//   shell.echo('Sorry, this script requires git');
+//   shell.exit(1);
+// }
 
-
-shell.echo('start 版本自增')
-if (shell.exec('npm version patch').code !== 0) {
-  shell.echo('Error: npm version failed');
-  shell.exit(1);
-}
-shell.echo('end 版本自增')
-
-
-shell.echo('start  编译代码')
-
-if (shell.exec('npm run build:lib').code !== 0) {
-  shell.echo('Error: npm run build:lib failed');
-  shell.exit(1);
-}
-shell.echo('end 编译代码')
+// shell.echo(git)
+// shell.echo('start 提交代码')
+// if (shell.exec('git add .').code !== 0) {
+//   shell.echo('Error: Git add failed');
+//   shell.exit(1);
+// }
+// //--no-verify 
+// if (shell.exec(`git commit -am "[release] Auto-commit" `).code !== 0) {
+//   shell.echo('Error: Git commit failed');
+//   shell.exit(1);
+// }
+// shell.echo('end 提交代码')
 
 
-shell.echo('start  发布npm')
-if (shell.exec('npm publish').code !== 0) {
-  shell.echo('Error: npm publish failed');
-  shell.exit(1);
-}
+// shell.echo('start 版本自增')
+// if (shell.exec('npm version patch').code !== 0) {
+//   shell.echo('Error: npm version failed');
+//   shell.exit(1);
+// }
+// shell.echo('end 版本自增')
 
-shell.echo('end 发布npm')
+
+// shell.echo('start  编译代码')
+
+// if (shell.exec('npm run build:lib').code !== 0) {
+//   shell.echo('Error: npm run build:lib failed');
+//   shell.exit(1);
+// }
+// shell.echo('end 编译代码')
+
+
+// shell.echo('start  发布npm')
+// if (shell.exec('npm publish').code !== 0) {
+//   shell.echo('Error: npm publish failed');
+//   shell.exit(1);
+// }
+// shell.echo('end 发布npm')
