60b9b0c64a59a4f945fa422ef1e9e88173d66949
WIN-838PA7AILD8\Administrator <hejia.song@zhaopin.com.cn>
2018-08-07 18:49:28 +0800

    [release] auto-commit

--- packages/index.js
+++ packages/index.js
@@ -7,7 +7,7 @@ import LayoutContainer from './layout-container';
 import LayoutItem from './layout-item';
 import MenuList from './menu-list';
 import TopNavBar from './top-nav-bar';
-const version = '0.1.43';
+const version = '0.1.44';
 import 'font-awesome/css/font-awesome.min.css'
 import './base.scss'
 const components = [
