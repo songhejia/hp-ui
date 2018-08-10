ecbee9518ab9ba43ebb586813047f02e0899c73d
WIN-838PA7AILD8\Administrator <hejia.song@zhaopin.com.cn>
2018-08-10 19:33:28 +0800

    是否可用

--- packages/job-type-dialog/index.vue
+++ packages/job-type-dialog/index.vue
@@ -17,27 +17,51 @@
                 <i :class="group.strKey===activeStrKey?'el-icon-caret-top':'el-icon-caret-bottom'"></i>
               </span>
             </div>
-            <template v-if="(index+1)%3===0||(index+1)===groups.length">
+            <template v-if="(index+1)%3===0||(index+1)===groups.length"
+                      v-for="seq in [2,1,0]">
               <div class="sub-item-group"
-                   :class="{['sub-item-group-'+groups[index-seq].strKey]:true,'is-expand':groups[index-seq].strKey===activeStrKey}"
-                   v-for="seq in [2,1,0]">
+                   :class="{['sub-item-group-'+groups[index-seq].strKey]:true,'is-expand':groups[index-seq].strKey===activeStrKey}">
+                <div class="arrow-up"
+                     :style="{'left':seq==2?'7%':seq==1?'40%':'73%'}">
+                  <i class="el-icon-caret-top"></i>
+                </div>
                 <div class="item"
                      v-if="groups[index-seq]&&groups[index-seq].children"
-                     v-for="group in groups[index-seq].children">
-                  <el-checkbox :label="group.strKey"
-                               class="check-item">{{group.value}}</el-checkbox>
+                     v-for="child in groups[index-seq].children">
+                  <el-checkbox :label="child.strKey"
+                               class="check-item"
+                               :class="{['check-item'+child.strKey]:true}"
+                               :disabled="checkedList.indexOf(groups[index-seq].strKey)>=0">{{child.value}}</el-checkbox>
                 </div>
               </div>
             </template>
           </template>
         </el-checkbox-group>
       </div>
+      <span slot="footer"
+            class="dialog-footer">
+        <div>
+          <!-- <el-tag v-for="tag in checkedList"
+                  :key="tag.name"
+                  closable
+                  :type="tag.type">
+            {{tag.name}}
+          </el-tag> -->
+
+        </div>
+        <div>
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary"
+                     @click="dialogVisible = false">确 定</el-button>
+        </div>
+      </span>
     </el-dialog>
   </div>
 </template>
 <script>
 import json from '../../local/job.type.json'
 import create from '../utils/create'
+import { returnStatement } from 'babel-types';
 export default create({
   name: 'job-type-dialog',
   data() {
@@ -49,12 +73,18 @@ export default create({
       activeStrKey: '-1'
     }
   },
+  computed: {
+    checkItem() {
+      return this.checkedList.join(',')
+    }
+  },
   methods: {
     ConvertGroups() {
       this.groups && (this.groups.length = 0)
       this.data.forEach(item => {
         const subItem = this.data.filter(i => item.strKey === i.parentStrKey)//找到孩子节点
         if (subItem && subItem.length > 0) {
+          item.children = []
           item.children.push(...subItem)
           this.groups.push(item)
         }
@@ -85,7 +115,7 @@ export default create({
     border-top: 1px solid #ccc;
     .item {
       display: inline-block;
-      padding: 2px 5px;
+      padding: 5px 5px;
       margin: 5px 0;
       width: 185px;
       box-sizing: border-box;
@@ -118,6 +148,14 @@ export default create({
       display: none;
       border: 1px solid #d8dce6;
       box-sizing: border-box;
+      position: relative;
+      .arrow-up {
+        position: absolute;
+        top: -15px;
+        font-size: 20px;
+        color: #eef1f6;
+        left: 7%;
+      }
     }
     .is-expand {
       display: block;
